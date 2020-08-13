442fcc17d411ea2ec235776ac2c8f48884479e34
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-08-13 22:24:31 +0200

    .

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -2,14 +2,16 @@ package search;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     final static Scanner scanner;
+    final static Map<String, Integer[]> queries;
+    static List<String> users;
 
     static {
         scanner = new Scanner(System.in);
+        queries = new LinkedHashMap<>();
     }
 
     public static void main(String[] args) {
@@ -22,38 +24,66 @@ public class Main {
             }
         }
 
-        String[] users = location == null ? catchUsers() : catchUsersFromFile(location);
+        users = location == null ? catchUsers() : catchUsersFromFile(location);
 
-        Map<String, Integer[]> map = new LinkedHashMap<>();
-        for (int i = 0; i < users.length; i++) {
-            String[] temp = users[i].toLowerCase().split(" ");
-            for (String data : temp) {
-                int len = map.get(data) == null ? 0 : map.get(data).length;
-                Integer[] nbrs = new Integer[len + 1];
-                for (int j = 0; j < len; j++) {
-                    nbrs[j] = map.get(data)[j];
-                } nbrs[len] = i;
-
-                map.put(data, nbrs);
-            }
-        }
+        makeMap();
 
         int option;
         do {
             switch (option = menu()) {
                 case 1:
-                    boolean foundSomeone = false;
-                    System.out.printf("\nEnter data to search people:\n");
-                    String que = scanner.nextLine().toLowerCase();
+                    System.out.println("Select a matching strategy: ALL, ANY, NONE");
+                    String strategy = scanner.nextLine();
 
-                    if (map.get(que) == null) {
-                        System.out.println("No matching people found.");
-                    } else {
-                        System.out.printf("%d persons found:\n", map.get(que).length);
-                        for (int line : map.get(que)) {
-                            System.out.println(users[line]);
-                        }
+                    System.out.printf("\nEnter a name or email to search all suitable people.\n");
+                    String[] queWords = scanner.nextLine().toLowerCase().split("\\s");
+
+                    Map<Integer, Integer> lines = new LinkedHashMap<>();
+
+                    int[] base = new int[users.size()];
+                    for (String queWord : queWords) {
+                        if (queries.get(queWord) == null) continue;
+                        for (int line : queries.get(queWord)) base[line]++;
                     }
+
+                    int count = 0;
+                    List<String> outUsers = new ArrayList<>();
+                    switch (strategy) {
+                        case "ALL":
+                            for (int i = 0; i < base.length; i++) {
+                                if (base[i] == queWords.length) {
+                                    outUsers.add(users.get(i));
+
+                                    count++;
+                                }
+                            }
+                            break;
+                        case "ANY":
+                            for (int i = 0; i < base.length; i++) {
+                                if (base[i] > 0) {
+                                    outUsers.add(users.get(i));
+
+                                    count++;
+                                }
+                            }
+                            break;
+                        case "NONE":
+                            for (int i = 0; i < base.length; i++) {
+                                if (base[i] == 0) {
+                                    outUsers.add(users.get(i));
+
+                                    count++;
+                                }
+                            }
+                            break;
+                    }
+
+                    System.out.printf("%d persons found:\n", count);
+
+                    for (String user : outUsers) {
+                        System.out.println(user);
+                    }
+
                     break;
                 case 2:
                     System.out.println();
@@ -65,35 +95,49 @@ public class Main {
         System.out.println("\nBye!");
     }
 
-    private static String[] catchUsers() {
+    private static void makeMap() {
+        for (int i = 0; i < users.size(); i++) {
+            String[] temp = users.get(i).toLowerCase().split(" ");
+            for (String data : temp) {
+                int len = queries.get(data) == null ? 0 : queries.get(data).length;
+                Integer[] nbrs = new Integer[len + 1];
+                for (int j = 0; j < len; j++) {
+                    nbrs[j] = queries.get(data)[j];
+                } nbrs[len] = i;
+
+                queries.put(data, nbrs);
+            }
+        }
+    }
+
+    private static ArrayList<String> catchUsers() {
         System.out.printf("\nEnter the number of people:\n");
         final int N = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Enter all people:");
-        String[] users = new String[N];
+        List<String> users = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            users[i] = scanner.nextLine();
+            users.add(scanner.nextLine());
         }
-        return users;
+        return (ArrayList<String>) users;
     }
 
-    private static String[] catchUsersFromFile(String location) {
+    private static ArrayList<String> catchUsersFromFile(String location) {
         File file = new File(location);
         try (Scanner scannerFile = new Scanner(file)) {
 
-            StringBuilder temp = new StringBuilder();
+            List<String> temp = new ArrayList<>();
             while (scannerFile.hasNextLine()) {
-                temp.append("\n");
-                temp.append(scannerFile.nextLine());
+                temp.add(scannerFile.nextLine());
             }
 
-            return temp.toString().split("\n");
+            return (ArrayList<String>) temp;
 
         } catch (final FileNotFoundException e) {
             System.out.printf("No file: %s", location);
         }
 
-        return new String[0];
+        return new ArrayList<String>();
     }
 
     private static int menu() {
@@ -108,6 +152,13 @@ public class Main {
     }
 
     private static void printArr(String[] users) {
+        System.out.println("=== List of people ===");
+        for (String user : users) {
+            System.out.println(user);
+        }
+    }
+
+    private static void printArr(List<String> users) {
         System.out.println("=== List of people ===");
         for (String user : users) {
             System.out.println(user);
